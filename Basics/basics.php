1f9cc4aea47f683e09fe3776e1f58d1f7b3e0813
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-04-02 15:22:02 +0600

    never stop

--- Basics/basics.php
+++ Basics/basics.php
@@ -1,2 +1,32 @@
+
 <?php
+// Sample
 echo "Hello World";
+echo "\n";
+$question = "How are you?";
+echo $question;
+
+// 1. Change the value
+echo "\n";
+$changeValue = 321;
+echo $changeValue;
+$changeValue = 3.213456789;
+echo "New Value of Variables is = {$changeValue}</br>";
+
+// var_dump ($name) 'var_dump is identify variable type'
+// // Type of data variable:
+// 1. Integer
+// 2. Boolean (True/false)
+// 3. string (Single coate double coate)
+// 4. float
+// 5. array
+// 6. object
+// 7. Resource
+echo "\n";
+
+$fname = "Sujon";
+$lname = "Shekh.";
+
+printf("His %s name is %s %s","Full",$fname,$lname);
+
+// BODMAS = BRACKET, OR, DIVISION, MULTI, ADDITION, SUB.
---
+++ Basics/echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    echo "<h1>Sujon Shekh</h1>";
+    $fname ="sujon shekh";
+    $lname ="Abdur Rob";
+    $number = 5;
+    $number += 5;
+    printf ("<h2>My first name is %s and Last name is %s<h2>",$fname,$lname);
+    printf ("Add = %d",$number);
+    ?>
+</head>
+<body>
+    
+</body>
+</html>

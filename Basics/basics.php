c73905940325b09879703204f3f7a4cf772520e9
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-04-02 16:52:48 +0600

    upadte

--- Basics/basics.php
+++ Basics/basics.php
@@ -29,4 +29,29 @@ $lname = "Shekh.";
 
 printf("His %s name is %s %s","Full",$fname,$lname);
 
-// BODMAS = BRACKET, OR, DIVISION, MULTI, ADDITION, SUB.
+//1. BODMAS = BRACKET, OR, DIVISION, MULTI, ADDITION, SUB.
+//2. Arithmatic operator = +, -, /, %, *
+//3. Operand is = 2,3,4
+//4. ++ Assign increasing factor
+echo "\n";
+ $n = 5;
+ $m = $n++;
+
+ echo "$m & $n";
+
+ echo "\n";
+ $o = 5;
+ $s = ++$o;
+
+ echo "$s and $o";
+
+//6. In number system if we provide '0' number before any number in variable it's will be octal number
+// as like $o = 017 =15;
+//7. for hexa $h = 0x1B;
+//8. Variable swapping
+echo "\n";
+$fisrtName = "Sujon";
+$middleName = "Abdur Rob";
+$LastName = "Shekh";
+
+printf('My 1st name is %3$s and middle name is %1$s and last name is %2$s',$fisrtName,$middleName,$LastName);
--- Basics/echo.php
+++ Basics/echo.php
@@ -5,13 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
+    // 1. Basics start numbers
     echo "<h1>Sujon Shekh</h1>";
     $fname ="sujon shekh";
     $lname ="Abdur Rob";
     $number = 5;
     $number += 5;
+    $fnumber = 2.88689752; 
+    $aNumber = 12.01;
+    $bnumber = 1.10;
+    $cnumber = 23.215;
+    $dnumber = 122.2358200;
+    printf ("%08.3f <br>",$aNumber);
+    printf ("%08.3f <br>",$bnumber);
+    printf ("%08.3f <br>",$cnumber);
+    printf ("%08.3f <br>",$dnumber);
+
     printf ("<h2>My first name is %s and Last name is %s<h2>",$fname,$lname);
-    printf ("Add = %d",$number);
+    printf ("Add = %d <br>",$number);
+    printf ("Your Result is = %.2f <br>",$fnumber);
+
+    // 2. Number converting system
+    
+    $product = 450000;
+    printf ('Number to Octal is %1$d to %1$o',$product); //when variables call it's name is Argument $product
     ?>
 </head>
 <body>

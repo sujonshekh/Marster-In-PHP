7fcb2dc1ec883ea3aec026ea4b91bbcdb55b60a1
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-04-03 14:59:28 +0600

    Update leap_year.php

--- Basics/leap_year.php
+++ Basics/leap_year.php
@@ -1,8 +1,8 @@
 <?php
 // leap year
 
-$year = 2000;
-if (($year % 4 == 0 && $year % 100 == 0) && $year % 400 == 0){
+$year = 2004;
+if ($year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)){
     echo "{$year} Leap year";
 }else {
     echo "{$year} is not Leap Year";

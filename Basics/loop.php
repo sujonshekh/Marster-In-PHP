ad551197237ded43b2d7c83fd67f83843a361331
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-04-08 23:10:56 +0600

    push

---
+++ Basics/loop.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i = 1; $i <= 10; $i++){
+    echo "\n";
+    echo "$i";
+    
+        for ($j = 1; $j < $i; $j++){
+            echo "#";
+        }
+    
+}
+echo "\n";
+for ($s = 1; $s <= 10; $s++ ){
+    echo "\n";
+    echo  "$s) " . "Sujon Shekh";
+}

6d549d326d9a40506a1d34db18c40ea94b063ac6
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-04-15 23:52:46 +0600

    update

--- Basics/loop.php
+++ Basics/loop.php
@@ -14,3 +14,19 @@ for ($s = 1; $s <= 10; $s++ ){
     echo "\n";
     echo  "$s) " . "Sujon Shekh";
 }
+echo PHP_EOL;
+// Multiple loop stepping
+echo PHP_EOL;
+for($t = 10, $v = 1; $t > 0; $t--, $v++){
+    echo $t.":".$v;
+    echo "\n";
+}
+// Nested loop
+echo "\n";
+
+// for ($a = 1; $a <=100; $a+=10){
+//     for ($b = $a; $b < $a+10; $b++){
+//         echo $b.' ';
+//     }
+ 
+// }

7ab5fa0b3a63edf5b943ce5a2b903cef33360a6c
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-05-10 12:52:27 +0600

    up

---
+++ array/array.php
@@ -0,0 +1,26 @@
+<?php 
+
+$age =[
+    "sujon" => 29,
+    "saima" => 19,
+    "rina"  => 45,
+    "babul" => 60
+];
+
+$age ["sujon"] = 30;
+
+var_dump($age);
+
+// push pop, shift, inshift...
+
+$sujon = [
+        30,
+        2008,
+        2010,
+
+];
+array_unshift ($sujon, 32);
+array_push ($sujon, 31);
+for ($i=0 ; $i < count($sujon); $i++){
+    echo $sujon[$i]."\n";
+}

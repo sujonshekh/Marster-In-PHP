428f157d98459deab38bd2482d9c9b039834b529
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-05-10 21:47:05 +0600

    Update

--- array/multidimensional_array.php
+++ array/multidimensional_array.php
@@ -1,16 +1,31 @@
 <?php
 
+$new = [
+    [45,"Tamin","IQBAL",45],
+    [65,"SUMON","SAKIB",75]
+];
+
+echo $new[1][3];
+echo "\n";
+for ($row =0; $row < 2; $row++){
+    for ($col =0; $col < 4; $col++){
+        echo $new [$row][$col];
+    }
+    echo  "\n";
+}
+
+// 2. Assosiative multidimensional array
 $emp = [
     [1,"Sujon","CSST",34550],
     [2,"Saima","S",500],
     [3,"Babul","Business",30000],
     [4,"Rina","HW",2000]
 ];
-echo $emp[2][1];
+
 echo "\n";
-for ($row =0; $row < 4; $row++){
-    for ($col =0; $col <4; $col++){
-        echo $emp [$row][$col];
+foreach ($emp as $v1){
+    foreach($v1 as $v2){
+        echo $v2 . " ";
     }
     echo  "\n";
 }

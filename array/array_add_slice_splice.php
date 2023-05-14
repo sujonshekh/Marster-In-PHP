5e665cf5ea688235825f71b9779d52524d40dada
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-05-14 17:34:13 +0600

    push

--- array/array_add_slice_splice.php
+++ array/array_add_slice_splice.php
@@ -8,6 +8,16 @@ $sujon = [
     'sokhi'=> 80,
     'dj'=> 81
 ];
-
+// in splice case we can't hold our key indexing
 $newSujon = array_splice($sujon, 2,2, ['teacher'=> 82, 'customer'=> 83]);
 print_r($sujon);
+
+// in this case we need to slice this case..
+
+$newSujonUpdated1 = array_slice($sujon,0,2);
+$newSujonUpdated2 = array_slice($sujon,4);
+$newSujonUpdated3 = $sujon =['teacher'=>82, 'customer'=>83];
+
+$newSujon1 = $newSujonUpdated1+$newSujonUpdated3+$newSujonUpdated2;
+
+print_r($newSujon1);
